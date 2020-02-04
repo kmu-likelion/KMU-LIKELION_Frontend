b7a09d1b46c1ab848b417dff6dc617fd1eb84190
labiss96 <cxz9080@gmail.com>
2020-02-05 03:46:35 +0900

    etc

--- frontend-react/src/api/api_board.js
+++ frontend-react/src/api/api_board.js
@@ -1,37 +1,34 @@
-import axios from "axios"
+import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/"
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
 
 export default {
+  /* Board CRUD api */
+  //모든글 불러오기
+  getAllPosts(url) {
+    console.log("getAllPosts 실행.");
+    return axios.get(`Board/${url}/`);
+  },
+  getPost(url, id) {
+    console.log("getPost 실행");
+    return axios.get(`Board/${url}/` + String(id));
+  },
 
-/* Board CRUD api */
-    //모든글 불러오기
-    getAllPosts(url) {
-        console.log('getAllPosts 실행.');
-        return axios.get(`Board/${url}/`);
-    },
-    getPost(url,id) {
-        console.log('getPost 실행');
-        return axios.get(`Board/${url}/`+String(id));
-    },
+  //글 생성
+  createPost(url, data) {
+    console.log("createPost 실행.");
+    return axios.post(`Board/${url}/`, data);
+  },
 
-    //글 생성
-    createPost(url,data) {
-        console.log('createPost 실행.');
-        return axios.post(`Board/${url}/`, data)
-    },
+  //글 수정
+  updatePost(url, id, data) {
+    console.log("updatePost 실행.");
+    return axios.put(`Board/${url}/` + String(id) + "/", data);
+  },
 
-    //글 수정
-    updatePost(url,id,data) {
-        console.log('updatePost 실행.');
-        return axios.put(`Board/${url}/`+String(id)+'/', data);
-    },
-    
-    //글 삭제
-    deletePost(url,id) {
-        console.log('deletePost 실행.');
-        return axios.delete(`Board/${url}/`+String(id));
-    }
-
-
-}
+  //글 삭제
+  deletePost(url, id) {
+    console.log("deletePost 실행.");
+    return axios.delete(`Board/${url}/` + String(id));
+  }
+};
--- frontend-react/src/components/board/NoticeDetail.jsx
+++ frontend-react/src/components/board/NoticeDetail.jsx
@@ -6,7 +6,7 @@ import moment from "moment";
 
 // @material-ui
 import Button from "@material-ui/core/Button";
-
+import TextField from "@material-ui/core/TextField";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -20,15 +20,22 @@ class NoticeDetail extends Component {
     id: "",
     title: "",
     body: "",
+    writer: "",
     pub_date: "",
-    run_date: ""
+    run_date: "",
+    input_cmt: "",
+    comments: []
   };
-
+  //...
   componentDidMount() {
     console.log("Detail ComponentDidMount");
     // this._getNotice(this.props.match.params.id);
     this.getNotice();
   }
+
+  // async getComments() {
+  //   await api.getAllPosts()
+  // }
 
   async getNotice() {
     await api
@@ -40,6 +47,7 @@ class NoticeDetail extends Component {
           title: data.title,
           body: data.body,
           id: data.id,
+          writer: data.writer,
           pub_date: moment(data.pub_date).format("YYYY-MM-DD hh:mm"),
           run_date: moment(data.run_date).format("YYYY-MM-DD")
         });
@@ -53,31 +61,74 @@ class NoticeDetail extends Component {
     document.location.href = "/notice";
   };
 
+  handlingChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  commentSubmit = async event => {
+    event.preventDefault();
+    console.log("body:", this.state.input_cmt);
+    console.log("board:", this.state.id);
+    console.log("writer:", this.state.writer);
+    let result = await api
+      .createPost("notice_comment", {
+        body: this.state.input_cmt,
+        board: this.state.id,
+        writer: this.state.writer
+      })
+      .catch(err => console.log(err));
+    console.log("정상적으로 생성됨.", result);
+    this.setState({ input_cmt: "" });
+    // this.getComments();
+  };
+
   render() {
     return (
-      <Card className={"card"}>
-        <CardContent>
-          <Typography>
-            Title : {this.state.title}
-            body : {this.state.body}
-            시각 : {this.state.run_date}
-            <br />
-            작성일 : {this.state.pub_date}
-          </Typography>
-        </CardContent>
+      <div>
+        <Card className={"card"}>
+          <CardContent>
+            <Typography>
+              Title : {this.state.title} <br />
+              body : {this.state.body} <br />
+              시각 : {this.state.run_date}
+              <br />
+              작성일 : {this.state.pub_date} <br />
+            </Typography>
+          </CardContent>
 
-        <CardActions>
-          <Button
-            color="secondary"
-            size="small"
-            onClick={event => this.handlingDelete(this.state.id)}
-          >
-            Delete
-          </Button>
-          <Link to={`/notice/update/${this.state.id}`}>Update</Link>
-          <Link to={"/notice"}>Back</Link>
-        </CardActions>
-      </Card>
+          <CardActions>
+            <Button
+              color="secondary"
+              size="small"
+              onClick={event => this.handlingDelete(this.state.id)}
+            >
+              Delete
+            </Button>
+            <Link to={`/notice/update/${this.state.id}`}>Update</Link>
+            <Link to={"/notice"}>Back</Link>
+          </CardActions>
+        </Card>
+
+        <Card className={"card"}>
+          <CardContent>
+            <h5>Comment</h5>
+            <form onSubmit={this.commentSubmit} className="commentForm">
+              <TextField
+                id="outlined-name"
+                label="comment"
+                name="input_cmt"
+                value={this.state.input_cmt}
+                onChange={this.handlingChange}
+                margin="normal"
+                // variant="outlined"
+              />
+              <Button type="submit" variant="contained" color="primary">
+                제출
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
     );
   }
 }
