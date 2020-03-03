f6075d7c7f5c7d53dbb554b4c28396ff8d116b69
jongmin park <showppark12@naver.com>
2020-03-03 13:37:53 +0900

    jongmin

--- frontend-react/src/components/accounts/mypage/LeftProfileView.jsx
+++ frontend-react/src/components/accounts/mypage/LeftProfileView.jsx
@@ -5,12 +5,20 @@ import Typography from "@material-ui/core/Typography";
 import MenuList from "@material-ui/core/MenuList";
 import MenuItem from "@material-ui/core/MenuItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-
+import EditIcon from '@material-ui/icons/Edit';
 import DraftsIcon from "@material-ui/icons/Drafts";
 import SendIcon from "@material-ui/icons/Send";
 import PriorityHighIcon from "@material-ui/icons/PriorityHigh";
 
 export default class LeftProfileView extends Component {
+  state={
+    img:null,
+  }
+  handleFileInput(e){
+    this.setState({
+      img : e.target.files[0],
+    })
+  }
   render() {
     const { username, sns_id, user_img } = this.props;
 
@@ -22,6 +30,28 @@ export default class LeftProfileView extends Component {
             alt="Myprofile"
             style={{ width: "70%", padding: "5px" }}
           />
+          <br />
+          <EditIcon type="button" data-toggle="modal" data-target="#exampleModal">
+          </EditIcon>
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">이미지 업데이트</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <input type="file" name="file" onChange={e=> this.handleFileInput(e)}/>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary" onClick={null}>Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
           <br />
           <br />
           <h1>{username}</h1>
