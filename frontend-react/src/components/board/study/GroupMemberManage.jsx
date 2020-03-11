db77431fc8000c29fc2024e3cac6240baf19eaba
jongmin park <showppark12@naver.com>
2020-03-11 23:47:43 +0900

    jongmin

--- frontend-react/src/components/board/study/GroupMemberManage.jsx
+++ frontend-react/src/components/board/study/GroupMemberManage.jsx
@@ -1,17 +1,6 @@
 import React, { Component } from "react";
-import api from "../../../api/GroupAPI";
-import { Link } from "react-router-dom";
-
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import TextareaAutosize from "@material-ui/core/TextareaAutosize";
-import Button from "@material-ui/core/Button";
-//import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-
-
 
 export default class GroupMemberManage extends Component {
   render() {
